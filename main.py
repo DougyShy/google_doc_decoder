86d7c78e2184456c10e9c0dc3f3a0c9108e63d6d
Doug Scheible <doug_scheible@yahoo.com>
2026-02-06 21:36:52 -0600

    working but with caveats

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://docs.google.com/document/d/e/2PACX-1vTMOmshQe8YvaRXi6gEPKKlsC6UpFJSMAk4mQjLm_u1gmHdVVTaeh7nBNFBRlui0sTZ-snGwZM4DBCT/pub"
+trial_url = "https://docs.google.com/document/d/e/2PACX-1vTMOmshQe8YvaRXi6gEPKKlsC6UpFJSMAk4mQjLm_u1gmHdVVTaeh7nBNFBRlui0sTZ-snGwZM4DBCT/pub"
+url = "https://docs.google.com/document/d/e/2PACX-1vSvM5gDlNvt7npYHhp_XfsJvuntUhq184By5xO_pA4b_gCWeXb6dM6ZxwN8rE6S4ghUsCj2VKR21oEP/pub"
+
 res = requests.get(url)
 soup = BeautifulSoup(res.text, "lxml")
 
@@ -21,75 +23,63 @@ for table in tables:
 
 # Group cells by threes
 parsed_google_doc = [doc_rows[i:i+3] for i in range(0, len(doc_rows), 3)]
-custom_parsed_google_doc = [doc_rows[i:i+3] for i in range(0, len(doc_rows), 3)]
+# Sort by x-coord
+parsed_google_doc.sort(key=lambda x: (int(x[0]), int(x[2])))
+# Swap x and y coords for actual screen writing and easier understanding
+for row in parsed_google_doc:
+    row[0], row[2] = int(row[2]), int(row[0])
+# Sort for same reason
+parsed_google_doc.sort(key=lambda r: (-int(r[0]), int(r[2])))
 
-# DECODE AND ULTIMATELY SET UP THE DRAWING OF GRID ENCRYPTED MESSAGE
+
 def decode_google_doc(doc):
-  number_of_rows_left = max(int(row[2]) for row in doc)
-  # - DEBUG/TESTING - print("number of rows left" + str(number_of_rows_left))
+    #for row in doc:
+    #    print(row)
+    number_of_rows = max(int(row[0]) for row in doc)
+    print(f'Number of rows left = {number_of_rows}')
+    # - DEBUG/TESTING - print("number of rows left" + str(number_of_rows_left))
+    max_chars_per_row = max(int(row[2]) for row in doc)
+    print(f'Max characters per line: {max_chars_per_row}')
+    number_of_coords = len(doc)
+    print(f'Number of coords: {number_of_coords}')
 
-  # swap grid logic to make it easier to traverse
-  for row in doc:
-    row[0], row[2] = row[2], row[0]
-  # sort for same reason
-  doc.sort(key=lambda r: (-int(r[0]), int(r[2])))
+    line = ''
+    line_rows = ''
 
-  # create individual rows to build words
-  row_text = ''
-
-  for current_row in range(number_of_rows_left, -1, -1):
-      line = ''
-      for row in doc:
-          if int(row[0]) == current_row:
-              line += row[1]  # keep building row string
-      row_text += line + '\n'  # to start a new line
-
-  print(row_text)
-
-# THIS IS THE ASSIGNMENT - ASSUMING ALL GOOGLE PUBLISHED URLs HAVE A HEADER ROW
-decode_google_doc(parsed_google_doc)
-
-# THIS IS TRIAL AND ERROR/DESIGN - IT INCORPORATES MORE THAN JUST ONE LETTER FROM THE EXAMPLE IF THIS IS THE FORMAT USED
-def load_custom_parsed(doc):
-    doc.append(['4', ' ', '2'])
-    doc.append(['5', '█', '2'])
-    doc.append(['6', '▀', '2'])
-    doc.append(['7', '▀', '2'])
-    doc.append(['8', '█', '2'])
-    doc.append(['3', ' ', '1'])
-    doc.append(['4', ' ', '1'])
-    doc.append(['5', '█', '1'])
-    doc.append(['6', '▀', '1'])
-    doc.append(['7', '█', '1'])
-    doc.append(['8', '▀', '1'])
-
-    doc.append(['1', ' ', '0'])
-    doc.append(['2', ' ', '0'])
-    doc.append(['3', ' ', '0'])
-    doc.append(['4', ' ', '0'])
-    doc.append(['5', '█', '0'])
-    doc.append(['6', ' ', '0'])
-    doc.append(['7', ' ', '0'])
-    doc.append(['8', '█', '0'])
-
-    doc.append(['9', ' ', '2'])
-    doc.append(['9', ' ', '1'])
-    doc.append(['9', ' ', '0'])
-
-    doc.append(['10', '█', '2'])
-    doc.append(['11', ' ', '2'])
-    doc.append(['12', ' ', '2'])
-    doc.append(['13', '█', '2'])
-    doc.append(['10', '▀', '1'])
-    doc.append(['11', '█', '1'])
-    doc.append(['12', '█', '1'])
-    doc.append(['13', '▀', '1'])
-    doc.append(['10', ' ', '0'])
-    doc.append(['11', '█', '0'])
-    doc.append(['12', '█', '0'])
-    doc.append(['13', ' ', '0'])
     
-# ELABORATE ON ORIGINAL LETTER TO FORM A WORD
-load_custom_parsed(custom_parsed_google_doc)
-# CHECK TO SEE IF WORD APPEARS SUCCESSFULLY WITH NEW CUSTOM GOOGLE PUB - * THE WORD 'FRY' SHOULD BE LEGIBLE INSTEAD OF JUST AN 'F'
-decode_google_doc(custom_parsed_google_doc)
+    current_index = -1
+    for y in range (number_of_rows, -1, -1):
+        current_index += 1
+        for x in range (0, max_chars_per_row -1):
+            print(y, x)
+            print(current_index)
+            if doc[current_index][0] == y and doc[current_index][2] == x:
+                print("match")
+                line += doc[current_index][1]
+                current_index += 1
+            else:
+                line += ' '
+        print(line)
+        line_rows += line + '\n'
+        line = ''
+        print(f'Current index: {current_index}')
+        print(f'Number of coords: {number_of_coords}')
+        
+
+    print(line_rows)
+
+    # ONLY GET TWO LINE UP TOP - BELOW WORKS - RESEARCH IT
+
+    '''coord_map = {(int(r), int(c)): ch for r, ch, c in doc}
+
+    text = ""
+    for y in range(number_of_rows, -1, -1):
+        line = ""
+        for x in range(max_chars_per_row):
+            line += coord_map.get((y, x), " ")
+        text += line + "\n"
+
+    print(text)'''
+        
+    
+decode_google_doc(parsed_google_doc)
